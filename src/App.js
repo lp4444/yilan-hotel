c31e24c36942bb3d4f2c4890ed0590c3aff51551
lp4444 <jack86520t@yahoo.com.tw>
2021-06-22 17:13:57 +0800

    finish deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import React, { useState, useContext, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import "./App.css";
